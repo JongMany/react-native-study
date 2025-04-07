728f155d4d0df7f28fd125f49410943e5751a590
JongMany <blackberry1114@naver.com>
2025-04-08 07:24:05 +0900

    feat input 이벤트 처리

--- client/App.tsx
+++ client/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
 
 function App(): React.JSX.Element {
   // const isDarkMode = useColorScheme() === 'dark';
+  const [name, setName] = useState('');
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    setName(text);
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>텍스트</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} />
-        <Text>텍스트</Text>
+        <Text>이름</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          onChangeText={handleChangeInput}
+        />
       </View>
     </SafeAreaView>
   );
@@ -28,20 +26,18 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
   },
   input: {
     flex: 1,
     borderWidth: 2,
     borderColor: 'black',
-    height: 100,
+    height: 50,
     width: 100,
   },
   inputContainer: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'red',
   },
 });
 
