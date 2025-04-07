961ddb3530c5d467a3c4f4ad8be532c20f3c76a4
JongMany <blackberry1114@naver.com>
2025-04-08 07:21:09 +0900

    feat: flexbox

--- client/App.tsx
+++ client/App.tsx
@@ -12,16 +12,14 @@ function App(): React.JSX.Element {
   // const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
         <Text>텍스트</Text>
-        <Button
-          title="버튼 이름"
-          onPress={() => {
-            console.log('클릭!');
-          }}
-        />
-        <TextInput />
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Text>텍스트</Text>
       </View>
     </SafeAreaView>
   );
@@ -29,10 +27,21 @@ function App(): React.JSX.Element {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  input: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: 'black',
+    height: 100,
+    width: 100,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: 'red',
-    // margin: '10%',
-    marginHorizontal: 10,
-    marginVertical: 10,
   },
 });
 
