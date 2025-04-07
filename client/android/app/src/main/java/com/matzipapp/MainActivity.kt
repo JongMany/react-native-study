398c586d444f2082d6a46500e2cb5c5921d9f5a7
JongMany <blackberry1114@naver.com>
2025-04-08 07:28:50 +0900

    fix: import 추가

--- client/android/app/src/main/java/com/matzipapp/MainActivity.kt
+++ client/android/app/src/main/java/com/matzipapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.matzipapp
 
 import com.facebook.react.ReactActivity
+import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
@@ -10,7 +11,7 @@ class MainActivity : ReactActivity() {
  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
-  
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
