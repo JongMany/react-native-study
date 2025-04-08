706f9f13e6416cfd814f23626f50b7d15d465415
JongMany <blackberry1114@naver.com>
2025-04-08 23:01:04 +0900

    feat: 커스텀 버튼 추가

--- client/src/screens/auth/AuthHomeScreen.tsx
+++ client/src/screens/auth/AuthHomeScreen.tsx
@@ -1,20 +1,23 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
-import {authNavigations} from '../../constants';
+import {authNavigations} from '../../constants/navigations';
+import CustomButton from '../../components/CustomButton';
 
 type AuthHomeScreenProps = StackScreenProps<AuthStackParamList, 'AuthHome'>;
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
     <SafeAreaView>
       <View>
-        <Button
-          title="로그인 화면으로 이동"
+        <CustomButton
+          label="로그인하기"
+          variant="filled"
           onPress={() => navigation.navigate(authNavigations.LOGIN)}
         />
-        <Button
-          title="회원가입"
+        <CustomButton
+          label="회원가입하기"
+          variant="outlined"
           onPress={() => navigation.navigate(authNavigations.SIGNUP)}
         />
       </View>
