8a5c8d44ae419dadb5659153ca5a2931b4d4dd2e
JongMany <blackberry1114@naver.com>
2025-04-10 13:15:45 +0900

    feat: location 관련 기능 추가

--- client/src/screens/map/MapHomeScreen.tsx
+++ client/src/screens/map/MapHomeScreen.tsx
@@ -4,10 +4,11 @@ import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
-import React from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import MapView, {LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import GeoLocation from '@react-native-community/geolocation';
 
 type Navigation = CompositeNavigationProp<
   StackNavigationProp<MapStackParamList>,
@@ -19,9 +20,49 @@ interface MapHomeScreenProps {}
 function MapHomeScreen({}: MapHomeScreenProps) {
   const inset = useSafeAreaInsets();
   const navigation = useNavigation<Navigation>();
+  const mapRef = useRef<MapView | null>(null);
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626929192,
+  });
+  const [isUserLocationError, setIsUserLocationError] = useState(false);
+
+  useEffect(() => {
+    GeoLocation.getCurrentPosition(
+      info => {
+        const {latitude, longitude} = info.coords;
+        setUserLocation({
+          latitude,
+          longitude,
+        });
+        setIsUserLocationError(false);
+      },
+      () => {
+        // Error
+        setIsUserLocationError(true);
+      },
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  }, []);
+
+  const handlePressUserLocation = () => {
+    if (isUserLocationError) {
+      return;
+    }
+    mapRef.current?.animateToRegion({
+      latitude: userLocation?.latitude,
+      longitude: userLocation?.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
   return (
     <>
       <MapView
+        ref={mapRef}
         style={styles.container}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
@@ -33,6 +74,11 @@ function MapHomeScreen({}: MapHomeScreenProps) {
         onPress={() => navigation.openDrawer()}>
         <Text>서랍</Text>
       </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <Text>내 위치</Text>
+        </Pressable>
+      </View>
     </>
   );
 }
@@ -54,6 +100,24 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.5,
     elevation: 4, // 안드로이드 shadow
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15,
+  },
+  mapButton: {
+    backgroundColor: colors.PINK_700,
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2, // 안드로이드 shadow
   },
 });
 
