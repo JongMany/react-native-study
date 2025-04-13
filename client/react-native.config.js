35f90076ae3fb3e8bcca97d6f319c313e0c770bf
JongMany <blackberry1114@naver.com>
2025-04-13 13:01:41 +0900

    chore: react-native-maps 버전 업그레이드

--- client/react-native.config.js
+++ client/react-native.config.js
@@ -6,5 +6,38 @@ module.exports = {
       },
     },
   },
+  project: {
+    android: {
+      unstable_reactLegacyComponentNames: [
+        'AIRMap',
+        'AIRMapCallout',
+        'AIRMapCalloutSubview',
+        'AIRMapCircle',
+        'AIRMapHeatmap',
+        'AIRMapLocalTile',
+        'AIRMapMarker',
+        'AIRMapOverlay',
+        'AIRMapPolygon',
+        'AIRMapPolyline',
+        'AIRMapUrlTile',
+        'AIRMapWMSTile',
+      ],
+    },
+    ios: {
+      unstable_reactLegacyComponentNames: [
+        'AIRMap',
+        'AIRMapCallout',
+        'AIRMapCalloutSubview',
+        'AIRMapCircle',
+        'AIRMapHeatmap',
+        'AIRMapLocalTile',
+        'AIRMapMarker',
+        'AIRMapOverlay',
+        'AIRMapPolygon',
+        'AIRMapPolyline',
+        'AIRMapUrlTile',
+        'AIRMapWMSTile',
+      ],
+    },
+  },
 };
- 
