4cc8165a1f6dbf576b273882015f074961a3a677
JongMany <blackberry1114@naver.com>
2025-04-17 07:54:39 +0900

    feat: MarkerModal 클릭 시 상세 피드로 넘어가도록 구현

--- client/src/components/map/MarkerModal.tsx
+++ client/src/components/map/MarkerModal.tsx
@@ -11,22 +11,34 @@ import {
 } from 'react-native';
 import React from 'react';
 import {useGetPost} from '@/hooks';
-import {colors} from '@/constants';
+import {colors, feedNavigations, mainNavigations} from '@/constants';
 import CustomMarker from '../common/CustomMarker';
 import Octicons from 'react-native-vector-icons/Octicons';
 import {getDateWithSeparator} from '@/utils';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {FeedStackParamList} from '@/navigations/stack/FeedStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawereNavigator';
 
 interface MarkerModalProps {
   markerId: number | null;
   isVisible: boolean;
   hide: () => void;
 }
+
+type Navigation = CompositeNavigationProp<
+  DrawerNavigationProp<MainDrawerParamList>,
+  StackNavigationProp<FeedStackParamList>
+>;
+
 export default function MarkerModal({
   markerId,
   isVisible,
   hide,
 }: MarkerModalProps) {
+  const navigation = useNavigation<Navigation>();
   const {data: post, isPending, isError} = useGetPost(markerId);
 
   if (!post || isPending || isError) {
@@ -38,10 +50,18 @@ export default function MarkerModal({
       ? 'http://10.0.2.2:3030'
       : 'http://localhost:3030';
 
+  const handlePressModal = () => {
+    // main navigation -> feed navigation
+    navigation.navigate(mainNavigations.Feed, {
+      screen: feedNavigations.FEED_DETAIL,
+      params: {id: post.id},
+    });
+  };
+
   return (
     <Modal visible={isVisible} transparent animationType="slide">
       <SafeAreaView style={styles.optionBackground} onTouchEnd={hide}>
-        <Pressable style={styles.cardContainer} onPress={() => {}}>
+        <Pressable style={styles.cardContainer} onPress={handlePressModal}>
           <View style={styles.cardInner}>
             <View style={styles.cardAlign}>
               {post && post.images.length > 0 && (
